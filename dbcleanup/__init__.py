6febe5be18f49a0c5bcbcc730e0ff9442997696a
Filipe Pina <fopina@gmail.com>
2022-03-23 18:13:46 +0000

    resolve django warnings for AutoField and default_app_config

--- dbcleanup/__init__.py
+++ dbcleanup/__init__.py
@@ -1,3 +1,10 @@
 __version__ = '0.1.1'
 
-default_app_config = 'dbcleanup.apps.DBCleanupConfig'
+# set default_app_config when using django earlier than 3.2
+try:
+    import django
+
+    if django.VERSION < (3, 2):
+        default_app_config = 'dbcleanup.apps.DBCleanupConfig'
+except ImportError:
+    pass
