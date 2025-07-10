5a1a18cadde871873233aa878cc494d31e394436
Fábio Pinto <fabio.pinto@blip.pt>
2025-07-10 10:30:23 +0100

    fix: single quotes

--- dbcleanup/__init__.py
+++ dbcleanup/__init__.py
@@ -1,10 +1,10 @@
-__version__ = "0.1.5"
+__version__ = '0.1.5'
 
 # set default_app_config when using django earlier than 3.2
 try:
     import django
 
     if django.VERSION < (3, 2):
-        default_app_config = "dbcleanup.apps.DBCleanupConfig"
+        default_app_config = 'dbcleanup.apps.DBCleanupConfig'
 except ImportError:
     pass
